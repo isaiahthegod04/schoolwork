69a065eab8681cc27e6f7e6ca5bf827d996fc07f
isaiahthegod04 <145810182+isaiahthegod04@users.noreply.github.com>
2025-01-20 15:20:12 -0800

    Update AList.h
    Homework assignmern A1: For the assignment, our implementation will be a C++ class called AList.  We will think of the list of items in an AList instance as having left and right ends, so the basic operations will be addLeft, addRight, deleteLeft and deleteRight.   So, for example, if L is an instance of the AList class, and the current list contents of L are <1,2,3>, then after executing the instruction L.addLeft(4), the list contents will be <4,1,2,3>.  If we then execute L.deleteRight(), the list contents will be <4,1,2>.

--- AList.h
+++ AList.h
@@ -1,6 +1,5 @@
 #ifndef ALIST_H
 #define ALIST_H
-
 #include <iostream>
 
 using namespace std;
@@ -8,113 +7,112 @@ using namespace std;
 template <typename Object>
 class AList 
 {
-  public:
+public:
     AList()
-      {  
-         theCapacity = 12 ;
-         objects = new Object[ theCapacity ]; 
-         theSize = 0 ;
-         left = 0 ; // array index of location just before the left-most list element  
-         right = 1 ; // array index just past the right-most list element
-      }
-      
-    ~AList( ) //destructor, deletes excess memory ----------- talk to TA about it
-      { delete [ ] objects; }
-
-    bool empty( ) const  { return size( ) == 0; }
-    int size( ) const { return theSize; }
-    void clear( ) {
-         theSize = 0 ;// sets item count to 0     
-         left = 0 ;   // sets the left pointer to 0      [  ,  ,  ,]
-         right = 1 ;  // sents the right pointer to 1    L^  R^
+    {  
+        theCapacity = 12;
+        objects = new Object[theCapacity]; 
+        theSize = 0;
+        left = 0;  // Index just before the left-most element
+        right = 0; // Index where the next right element will go
+    }
+    
+    ~AList() //destructor, deletes excess memory ----------- talk to TA about it
+    { delete[] objects; }
+    bool  empty() const { return theSize == 0; }
+    int size() const { return theSize; }
+    void clear()  {
+        theSize = 0; // sets item count to 0   
+        left = 0; // sets the left pointer to 0      [  ,  ,  ,]
+        right = 0;// sets the right pointer to 1     L^  R^
     }
 
-    // Operations 
 
+// Opertions are below 
+// should these be integers whty is is x?
+    void addLeft(const Object x){
+        // check this again
+        left = (left - 1 + theCapacity) % theCapacity; // Move left pointer backward
+        objects[left] = x; // Assign value
+        theSize++; // Increment size
+    }
 
-
-// shouldn't these be integers?
-    void addLeft( const Object x )// Insert a new object at the left end 
+    void addRight(const Object x)// puts opject on right end
     {
-
-	// Check This
-
-       theSize ++ ;
-	left = (left - 1 + theCapacity) % theCapacity;
-        objects[left] = x;
+        objects[right] = x; // Assign value
+        right = (right + 1) % theCapacity; // Move right pointer forward
+        theSize++; // Increment size
     }
 
-    void addRight( const Object x )// Insert a new object at the right end
+    Object removeLeft()// remove and return right end
     {
-        theSize ++ ;
-	objects[right] = x ;
-        right = (right+1) % theCapacity ;
+        if (empty()) {
+            cout << "Nothing to remove" << endl;
+            return Object(); // Return this if theres nothing there
+        }
+        Object removed = objects[left]; // remove the leftmost value
+        left = (left + 1) % theCapacity; // Move left pointer forward
+        theSize--; // Decrement size
+        return removed;
     }
 
-
-
-    Object removeRight( )// Remove and return the object at the right end
+    Object removeRight()
     {
-         if(empty()) {cout << "Nothing to remove"}; // if theres nothing in the list display this
-
-        right = (right - 1 + theCapacity) % theCapacity; // moves the pointer over one space LEFT, then adds the capacity so it can't have negative value, & includes modular feature so it wraps
-        Object RmvRight = objects[right];// removed the right object
-        theSize--;// decreased total size by one
-        return RmvRight; //returns the removed object
+        if (empty()) {
+            cout << "Nothing to remove" << endl;
+            return Object(); // Return this if theres nothing there 
+        }
+        right = (right - 1 + theCapacity) % theCapacity; // Move right pointer backward
+        Object removed = objects[right]; // Get the rightmost value
+        theSize--; // Decrement size
+        return removed;
     }
 
-
-
-    Object removeLeft( )// Remove and return the object at the left end
+    void display() const // prints everythin gthats in the alist
     {
-        if(empty()) {cout << "Nothing to remove"};
-
-        left = (left + 1) % theCapacity;// just moves the pointer one space RIGHT 
-        Object RmvLeft = objects[left];
-        theSize--;// decreased size by one 
-        return RmvLeft;
-    }
-
-
-
-    void display() const // print out the contents of the Alist
-  {
-
-     if(empty()) {cout << "Nothing in Alist"};// if its emplty print this
-
-
-      int Lpointer = left // rename make understanding easier 
-       for (int i = 0; i < theSize; ++i)
-         cout << objects[Lpointer] << " ";
-       Lpointer =  (Lpointer + 1) % theCapacity; 
-      // this loop prints all the elements
+        if (empty()) {
+            cout << "Nothing in AList" << endl;
+            return;// if empty print this
+        }
+        int Lpointer = left;
+        for (int i = 0; i < theSize; ++i) {
+            cout << objects[Lpointer] << " ";
+            Lpointer = (Lpointer + 1) % theCapacity; // Move forward in circular array
+        }
+           // this loop prints all the elements
       // i represents the number of loop cycles it takes
       // obvi can't have more loop cycles then there is objects in section
       // it continues the loop if the number of current loops is < the size   
+        cout << endl;
     }
 
-
-    void ddisplay() const // print out the contents of the objects 
+    void ddisplay() const// print out the contents of the objects 
     // array, and relevant variables, for debugging or verifying 
     // correctness. 
-    {   cout << "the elements in the list are: ";
+    {
+        cout << "The elements in the list are: ";
         display();
-        cout << "the other junk is: ";
-        for (int i = 0; i < theCapacity; ++i)
-        // this loop cycles thru the array 0 to 11 (the capacity - 1) times
-          cout << objects[i] << " ";
-// for each cycle, it prints whatever object is there, no matter what it is.
-          cout <<  "The size of the list is: " << theSize << "The left element is: " << left << " The right element is: " << right << endl;
+        cout << "The internal array is: ";
+        for (int i = 0; i < theCapacity; ++i) {
+                    // this loop cycles thru the array 0 to 11 (the capacity - 1) times
+
+            cout << objects[i] << " ";
+            // for each cycle, it prints whatever object is there, no matter what it is.
+
+        }
+        cout << endl;
+        cout << "The size of the list is: " << theSize << endl;
+        cout << "The left pointer is: " << left << endl;
+        cout << "The right pointer is: " << right << endl;
     }
     // prints info about list
 
-
-  private:
+private:
     int theSize;
     int left;
     int right;
     int theCapacity;
-    Object * objects;
+    Object* objects;
 };
 
 #endif
